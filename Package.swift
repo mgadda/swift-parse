b80ab30f4b8bcd51cdb8178ccf0b85bd62b808b4
Matt Gadda <mgadda@gmail.com>
2017-01-07 11:35:16 -0800

    add dependency on swift-ext

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftParse"
+  name: "SwiftParse",
+  dependencies: [
+    .Package(url: "https://github.com/mgadda/swift-ext.git", majorVersion: 0)
+  ]
 )
