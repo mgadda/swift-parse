0b62841d23b122ff66a58595bb2b97c4be740e96
Matt Gadda <mgadda@gmail.com>
2017-01-23 11:19:48 -0800

    add assertNotParsed helper

--- Tests/SwiftParseTests/ParserHelpers.swift
+++ Tests/SwiftParseTests/ParserHelpers.swift
@@ -14,16 +14,27 @@ protocol ParserHelpers {
 
 extension ParserHelpers {
   func assertParsed<T: Equatable, U: Equatable>(
-    _ parser: ([U]) -> (T, [U])?,
-    input: [U],
-    _ val: T,
-    _ next: [U],
+    _ parser: ([T]) -> (U, [T])?,
+    input: [T],
+    _ val: U,
+    _ remaining: [T],
     _ message: @autoclosure () -> String = "",
     file: StaticString = #file,
     line: UInt = #line
   ) {
-    let result: (T, [U])? = parser(input)
+    let result: (U, [T])? = parser(input)
+    XCTAssertNotNil(result)
     XCTAssertEqual(result!.0, val, message, file: file, line: line)
-    XCTAssertEqual(result!.1, next, message, file: file, line: line)
+    XCTAssertEqual(result!.1, remaining, message, file: file, line: line)
+  }
+
+  func assertNotParsed<T: Equatable, U: Equatable>(
+    _ parser: ([T]) -> (U, [T])?,
+    input: [T],
+    _ message: @autoclosure () -> String = "",
+    file: StaticString = #file,
+    line: UInt = #line
+  ) {
+    XCTAssertNil(parser(input))
   }
 }
